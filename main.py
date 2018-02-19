043a9a16a4d46664ef27ed33e32ca1d695e40e44
Vatsal Kanakiya <vatsal.kanakiya@somaiya.edu>
2018-02-19 15:59:14 +0530

    Created root window

--- main.py
+++ main.py
@@ -2,8 +2,16 @@
 
 from tkinter import *
 
+class Window(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+
 def main():
-    
+    root = Tk()
+    app = Window(root)
+    root.mainloop()
     return
 
 if __name__ == '__main__':
